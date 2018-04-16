2c5c6f60aa35b5891743a22e3f09e61863f445ca
Yoshiki Ogiwara <y.ogiwara.1214@gmail.com>
2018-04-16 14:54:01 +0900

    added create method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,21 @@
 class TweetsController < ApplicationController
 
 	def index
-		@tweets = Tweet.include(:user).order(created_at 'DESC')
+		@tweets = Tweet.all
+		#@tweets = Tweet.include(:user).order(created_at 'DESC')
 	end
 
 	def new
-		
 	end
 
 	def create
-
+		@tweet = Tweet.create(image: tweet_params[:image], text: tweet_params[:image], user_id: current_user.id)
 	end
+
+		private
+		def tweet_params
+			params.permit(:image, :text)
+		end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415091847) do
+ActiveRecord::Schema.define(version: 20180416051345) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string   "nickname",   limit: 255
     t.text     "text",       limit: 65535
     t.text     "image",      limit: 65535
     t.integer  "user_id",    limit: 4
