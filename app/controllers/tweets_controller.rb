54e13ebaf6f80e4a4ca99231bfdf8d61f42faeb1
Yoshiki Ogiwara <y.ogiwara.1214@gmail.com>
2018-04-16 13:58:41 +0900

    create new method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,15 @@
 class TweetsController < ApplicationController
 
 	def index
-		@tweets = Tweet.include(:user).order('DESC')
+		@tweets = Tweet.include(:user).order(created_at 'DESC')
+	end
+
+	def new
+		
+	end
+
+	def create
+
 	end
 
 end
