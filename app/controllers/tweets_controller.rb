cf662435e694ec200e0074c78e55415088ceabb9
Yoshiki Ogiwara <y.ogiwara.1214@gmail.com>
2018-04-16 20:27:28 +0900

    my page done

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,14 +3,14 @@ class TweetsController < ApplicationController
 	before_action :authenticate_user!, except: :index
 
 	def index
-		@tweets = Tweet.page(params[:page]).per(5).order("created_at DESC")
+		@tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
 	end
 
 	def new
 	end
 
 	def create
-		@tweet = Tweet.create(image: tweet_params[:image], text: tweet_params[:text]) #user_id: current_user.id)
+		@tweet = Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
 	end
 
 		private
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "tweets#index"
   devise_for :users
   resources :tweets
+  resources :users, only: [:show]
 
 end
